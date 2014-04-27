d58044fa0ecfb60dc15874eaf21e45c9dd13aa4e
anumaz <anthodupont@gmail.com>
2014-04-27 15:09:39 -0400

    Added outputchatboxs to understand whats up

--- game/resources/weapons-attach/s_weaponattach.lua
+++ game/resources/weapons-attach/s_weaponattach.lua
@@ -19,11 +19,12 @@ addEventHandler( "onPlayerWeaponSwitch", getRootElement(),
 				ak47 = createObject(355, 0, 0, 0)
 				exports.bone_attach:attachElementToBone(ak47,source,3,0.1,0.25,0,0,45,180)
 				ak47s[source] = ak47
-				outputChatBox("Now in your back", source)
+				outputChatBox("AK is now added", source)
 			elseif (getPedWeapon(source, 5) == 30) and (getPedWeapon(source) ~= 30) and (getElementData(source, "isHolding30") == 0) then
 				setElementData(source, "isHolding30", 1)
 				exports.bone_attach:detachElementFromBone(ak47s[source])
 				destroyElement(ak47s[source])
+				outputChatBox("AK is now removed", source)
 			end
 			-- Nightstick
 			if (getPedWeapon(source, 1) == 3) and (getPedWeapon(source) == 3) then
@@ -31,12 +32,12 @@ addEventHandler( "onPlayerWeaponSwitch", getRootElement(),
 				nightstick = createObject(334, 0, 0, 0)
 				exports.bone_attach:attachElementToBone(nightstick,source,3,0,0,0,0,0,0)
 				nightsticks[source] = nightstick
-				outputChatBox("Now in your back", source)
+				outputChatBox("Nightstick is now added", source)
 			elseif (getPedWeapon(source, 1) == 3) and (getPedWeapon(source) ~= 3) and (getElementData(source, "isHolding3") == 0) then
 				setElementData(source, "isHolding3", 1)
 				exports.bone_attach:detachElementFromBone(nightsticks[source])
 				destroyElement(nightsticks[source])
-				outputChatBox("should be deleted now...",source)
+				outputChatBox("Nightstick is now removed",source)
 			end
 		end
 	end
